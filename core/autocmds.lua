6c359721b3f637fe72f839b6e4ecff7f5e5e1e77
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2023-04-18 22:05:38 +0300

    Show line diagnostics in hover window

---
+++ core/autocmds.lua
@@ -0,0 +1,2 @@
+-- Show line diagnostics automatically in hover window
+vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
--- core/options.lua
+++ core/options.lua
@@ -27,3 +27,5 @@ opt.cursorline = true -- highlight the current cursor line
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+
+vim.o.updatetime = 250
