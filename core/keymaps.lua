5525035b67dabc4f990336106e1952fe180e3f3c
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2023-06-29 09:20:05 +0300

    Add find diagnostics

--- core/keymaps.lua
+++ core/keymaps.lua
@@ -18,6 +18,7 @@ if status then
 	keymap.set("n", "<leader>fg", builtin.live_grep, {})
 	keymap.set("n", "<leader>fb", builtin.buffers, {})
 	keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	keymap.set("n", "<leader>fe", builtin.diagnostics, {})
 end
 
 -- my command
